d365f0ddcaed66e6a7b69b02818bde12f25943c1
GERMAN ARCINIEGAS <germanarciniegas@MacBook-Air-de-GERMAN.local>
2023-11-08 21:21:37 -0500

    Filter by name

--- src/App.js
+++ src/App.js
@@ -19,18 +19,86 @@ const sample = {
 
 function App() {
   const[listCryptos, setListCryptos] = React.useState([]);
+  const[displayCryptos, setDisplayCryptos] = React.useState([]);
+  const[search, setSearch] = React.useState("");
+
+  // const[shouldUpdate, setShouldUpdate] = React.useState(false);
+
+
+  // const myFunction = React.useCallback((parametro1, parametro2) => {
+
+  // },[shouldUpdate]);
+
+  // const myObjeto = React.useMemo({}, [shouldUpdate]);
+
+  // const myObjeto1 = React.useRef(()=> 4+5);
+  const miInput = React.useRef(null);
+
+  React.useEffect(()=>{
+    if(miInput&& miInput.current){
+      miInput.current.addEventListener('keyup', (e)=>{
+        console.log(e.target.value);
+        setSearch(e.target.value);
+      })
+    }
+  },[])
+
+  React.useEffect(()=>{
+    const filteredData = listCryptos.filter((crypto) => {
+      console.log(crypto.name, search)
+      return crypto.name.toLowerCase().includes(search.toLowerCase()) ||
+      crypto.symbol.toLowerCase().includes(search.toLowerCase())
+    });
+    setDisplayCryptos(filteredData);
+  },[search])
+
+  // React.useEffect(()=> {
+  //   console.log("Render function");
+  // },[myFunction, myObjeto])
+
+  ///{} === {} false
+  ///()=>{} === ()=>{} false
 
   React.useEffect(()=> {
     fetch("https://api.coincap.io/v2/assets")
       .then((response) => response.json())
-      .then((dataJson) => {setListCryptos(dataJson.data)});
+      .then((dataJson) => {
+        setListCryptos(dataJson.data);
+        setDisplayCryptos(dataJson.data);
+      });
   },[])
+
+  React.useEffect(()=>{
+    console.log("Disparo de Effect");
+    // Updating Render, monting render
+  })
+
+  React.useEffect(()=>{
+    console.log("Disparo de Effect did mounting");
+    // Render, monting, did mount
+   // componentDidMount()
+  },[])
+
+
+  // React.useEffect(()=>{
+  //   console.log("Disparo de Effect updating");
+  //   // Render, monting, did monting
+  //   setTimeout(() => {
+  //     setShouldUpdate(true)
+  //   }, 2000);
+  // },[shouldUpdate])
+
+  // React.useEffect(()=>{
+  //   return () => {return null}
+  //   //componentWillUnmount()
+  // },[])
 
   return (
     <div className="App">
       <h1>Ranking Cryptos Modulo 3</h1>
+      <input type="text" ref={miInput}/>
       <p>This is a React App Made in js to Deplot in Vercel credits to CoinCap</p>
-      {listCryptos.map((crypto) => <ItemCrypto 
+      {displayCryptos.map((crypto) => <ItemCrypto 
             symbol={crypto.symbol}
             price={crypto.priceUsd}
             name={crypto.name}
